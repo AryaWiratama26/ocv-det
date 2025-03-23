9252201471a286567678dedc80ec402a67d3057c
Arya Wiratama <aryawiratama2401@gmail.com>
2025-03-23 19:12:15 +0700

    eye detection open cv

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+
+eye_ref = cv2.CascadeClassifier("haarcascade_eye.xml")
+camera = cv2.VideoCapture(0)
+
+def eye_detection(frame):
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    eyes = eye_ref.detectMultiScale(grey_frame, scaleFactor=1.1)
+    return eyes
+
+def frame_box(frame):
+
+    for x,y,w,h in eye_detection(frame):
+        cv2.rectangle(frame,(x,y),(x + w, y + h), (255, 255, 255), 5)
+
+def main():
+    
+    while True:
+        result, frame = camera.read()
+        frame_box(frame)
+        cv2.imshow("Eye Detection", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+if __name__ == "__main__":
+    main()
